b1de467eb08fabec3298649499c4a77eda04f56f
Samuel <smltrs0@gmail.com>
2024-09-11 17:27:22 -0500

    Refactorizar componente PaymentModal para mejorar la estructura y la legibilidad del código

--- frontend/src/components/PaymentModal.jsx
+++ frontend/src/components/PaymentModal.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import PaymentMethodSelection from './PaymentModal/PaymentMethodSelection';
-import PaymentDetails from './PaymentModal/PaymentDetails';
-import PaymentConfirmation from './PaymentModal/PaymentConfirmation';
+
 import ModalHeader from './PaymentModal/ModalHeader';
 import ModalSteps from './PaymentModal/ModalSteps';
 import ModalNavigation from './PaymentModal/ModalNavigation';
+import RenderStepContent from './PaymentModal/RenderStepContent';
 
 const PaymentModal = ({ isOpen, onClose }) => {
 	const [step, setStep] = useState(1);
-	const [paymentMethod, setPaymentMethod] = useState('');
 
 	const nextStep = () => setStep((prevStep) => Math.min(prevStep + 1, 3));
 	const prevStep = () => setStep((prevStep) => Math.max(prevStep - 1, 1));
@@ -19,23 +17,6 @@ const PaymentModal = ({ isOpen, onClose }) => {
 		onClose();
 	};
 
-	const renderStepContent = () => {
-		switch (step) {
-			case 1:
-				return (
-					<PaymentMethodSelection
-						setPaymentMethod={setPaymentMethod}
-					/>
-				);
-			case 2:
-				return <PaymentDetails paymentMethod={paymentMethod} />;
-			case 3:
-				return <PaymentConfirmation />;
-			default:
-				return null;
-		}
-	};
-
 	if (!isOpen) return null;
 
 	return (
@@ -43,7 +24,9 @@ const PaymentModal = ({ isOpen, onClose }) => {
 			<div className="bg-white rounded-lg shadow-lg w-11/12 sm:w-11/12 md:w-3/4 lg:w-1/2 p-4 md:p-6 max-h-screen overflow-y-auto">
 				<ModalHeader closeModal={closeModal} />
 				<ModalSteps step={step} />
-				<div>{renderStepContent()}</div>
+				<div>
+					<RenderStepContent step={step} />
+				</div>
 				<ModalNavigation
 					step={step}
 					prevStep={prevStep}
