245e0e057165c1d9124784a7ca3522113e4c1cc6
Samuel <smltrs0@gmail.com>
2024-09-11 22:34:39 -0500

    Feat: Aceptacion de terminos y condiciones

--- frontend/src/components/PaymentModal/RenderStepContent.jsx
+++ frontend/src/components/PaymentModal/RenderStepContent.jsx
@@ -5,10 +5,27 @@ import PaymentMethodSelection from './PaymentMethodSelection';
 import PaymentDetails from '../PaymentModal/PaymentDetails';
 import PaymentConfirmation from '../PaymentModal/PaymentConfirmation';
 
-const RenderStepContent = ({ step = 1 }) => {
+const RenderStepContent = ({ step, handleTermsChange }) => {
 	const [paymentMethod, setPaymentMethod] = useState('');
 
 	switch (step) {
+		case 0:
+			return (
+				<div className="mt-4">
+					<label onChange={handleTermsChange}>
+						<input type="checkbox" className="mr-2" />{' '}
+						Acepto los términos y condiciones descritas
+						en el siguiente link{' '}
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://wompi.com/assets/downloadble/reglamento-Usuarios-Colombia.pdf"
+						>
+							terminos y condiciones
+						</a>
+					</label>
+				</div>
+			);
 		case 1:
 			return (
 				<PaymentMethodSelection
@@ -26,6 +43,7 @@ const RenderStepContent = ({ step = 1 }) => {
 
 RenderStepContent.propTypes = {
 	step: PropTypes.number.isRequired,
+	handleTermsChange: PropTypes.func.isRequired,
 };
 
 export default RenderStepContent;
