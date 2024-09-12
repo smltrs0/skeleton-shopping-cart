fb94a2993d8488643cdedcc51931e0cab8c08eeb
Samuel <smltrs0@gmail.com>
2024-09-11 22:26:06 -0500

    Feat: implementacion de lazy loading para las imagenes

--- frontend/src/components/ProductList/ProductImage.jsx
+++ frontend/src/components/ProductList/ProductImage.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const ProductImage = ({ src, alt }) => {
 	return (
 		<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:border group-hover:bg-slate-50 lg:h-80 transition duration-300">
-			<img
+			<LazyLoadImage
 				src={src}
 				alt={alt}
 				className="h-full w-full object-cover object-center lg:h-full lg:w-full"
