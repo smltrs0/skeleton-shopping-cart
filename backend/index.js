de33de804af149c6f98225bb1a90bec4957e11ca
Samuel <smltrs0@gmail.com>
2024-09-11 22:26:36 -0500

    Feat: configuracion de cors

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ import bodyParser from 'body-parser';
 import paymentRoutes from './src/infrastructure/routes/paymentRoutes.js';
 import itemsRoutes from './src/infrastructure/routes/itemsRoutes.js';
 import setupSwagger from './src/swagger.js';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/api', paymentRoutes);
-app.use('/api', paymentRoutes);
+app.use('/api', itemsRoutes);
 
+app.use(cors());
 // Configurar Swagger
 setupSwagger(app);
 
